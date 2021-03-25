9b262bd3041163c5d20b5e8f5ce81bf92571227f
unknown <alexandre.netchaev@epita.fr>
2021-03-25 20:09:41 +0000

    book description

---
+++ Models/Book_description.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DorsetBookApi.Models
+{
+    public class Book_description
+    {
+        [Key]
+        public int id { get; set; }
+        public int book_id { get; set; }
+        public string book_name { get; set; }
+        public string book_description { get; set; }
+    }
+}
