eba8bade3f0f60e280e1afdc5a3a47d1bf077ded
Baptiste Nehmé <baptiste.nehme@epita.fr>
2021-03-30 00:59:41 +0100

    Modifed project template to webapi

--- Startup.cs
+++ Startup.cs
@@ -2,14 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DorsetBookApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using DorsetBookApi.Data;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace DorsetBookApi
 {
@@ -25,10 +28,15 @@ namespace DorsetBookApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+
+            services.AddControllers();
             services.AddDbContext<Context>(opt =>
             {
                 opt.UseMySql(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DorsetBookApi", Version = "v1" });
             });
         }
 
@@ -38,15 +46,11 @@ namespace DorsetBookApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DorsetBookApi v1"));
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -54,9 +58,7 @@ namespace DorsetBookApi
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
         }
     }
