1967c0391babd77a48de79a37e98979339da8794
unknown <alexandre.netchaev@epita.fr>
2021-03-30 17:38:56 +0100

    userservices

---
+++ Helpers/AppException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DorsetBookApi.Helpers
+{
+    public class AppException
+    {
+    }
+}
