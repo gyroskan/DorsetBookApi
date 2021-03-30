1b3113a7e3befeffdbcdd9b985a58d94a05d886d
Baptiste Nehmé <baptiste.nehme@epita.fr>
2021-03-30 01:10:46 +0100

    Added User Model

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DorsetBookApi.Models
+{
+    public class User
+    {
+        [Key]
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string username { get; set; }
+        public string passwordhash { get; set; }
+    }
+}
