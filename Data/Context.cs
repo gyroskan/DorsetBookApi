69acd048ec9f99369cb2be1fe3cf10473d2c3a16
Baptiste Nehmé <baptiste.nehme@epita.fr>
2021-03-30 01:19:03 +0100

    Added users models and dto

--- Data/Context.cs
+++ Data/Context.cs
@@ -8,5 +8,6 @@ namespace DorsetBookApi.Data
         public Context(DbContextOptions<Context> options) : base(options) { }
         public DbSet<Book> Book { get; set; }
         public DbSet<Book_description> Book_Description { get; set; }
+        public DbSet<User> User { get; set; }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DorsetBookApi.Models
 {
+    [Table("user")]
     public class User
     {
         [Key]
